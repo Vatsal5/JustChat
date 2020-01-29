b9e70697143e6b517785b1a11c17108b976587a9
Vatsal5 <aggarwal.vatsal5@gmail.com>
2020-01-29 18:36:43 +0530

    Initial commit

---
+++ app/src/main/java/com/androidstudio/chattingapp/Registration.java
@@ -0,0 +1,112 @@
+package com.androidstudio.chattingapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class Registration extends AppCompatActivity {
+
+    EditText etPhone;
+    Button btnVerify;
+
+    String phone;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    PhoneAuthProvider.ForceResendingToken token;
+    String VerificationId;
+
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        etPhone = findViewById(R.id.etPhone);
+        btnVerify = findViewById(R.id.btnSubmit);
+
+        dialog = new ProgressDialog(this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCancelable(false);
+
+
+        if(FirebaseAuth.getInstance().getCurrentUser() != null)
+        {
+            startActivity(new Intent(Registration.this,MainActivity.class));
+        }
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                Log.d("myapp","Verification Completed");
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(Registration.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+
+                VerificationId = s;
+                token = forceResendingToken;
+
+                Intent intent = new Intent(Registration.this,Verification.class);
+                intent.putExtra("VerificationId",VerificationId);
+                intent.putExtra("token",token);
+                intent.putExtra("phone",phone);
+                startActivity(intent);
+                dialog.dismiss();
+            }
+        };
+
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phone = "+91"+etPhone.getText().toString();
+
+                if(phone.isEmpty())
+                    Toast.makeText(Registration.this, "Please enter phone number", Toast.LENGTH_LONG).show();
+                if(phone.length() !=13)
+                    Toast.makeText(Registration.this, "Enter Valid Phone number", Toast.LENGTH_LONG).show();
+
+                if(!phone.isEmpty() && phone.length() ==13)
+                {
+                    dialog.setTitle("Sending OTP");
+                    dialog.setMessage("Please Wait");
+                    dialog.show();
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            phone,        // Phone number to verify
+                            60,                 // Timeout duration
+                            TimeUnit.SECONDS,   // Unit of timeout
+                            Registration.this,               // Activity (for callback binding)
+                            mCallbacks);        // OnVerificationStateChangedCallbacks
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/androidstudio/chattingapp/Verification.java
@@ -0,0 +1,151 @@
+package com.androidstudio.chattingapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class Verification extends AppCompatActivity {
+
+    EditText etOTP;
+    Button btnVerify;
+    TextView tvResend;
+    ProgressDialog dialog;
+    String VerificationId;
+    PhoneAuthProvider.ForceResendingToken token;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    String phone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+
+        etOTP = findViewById(R.id.etOTP);
+        btnVerify = findViewById(R.id.btnVerify);
+        tvResend = findViewById(R.id.tvResend);
+
+        VerificationId = getIntent().getStringExtra("VerificationId");
+        token = (PhoneAuthProvider.ForceResendingToken) getIntent().getSerializableExtra("token");
+
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Verifying");
+        dialog.setMessage("Please Wait..");
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCancelable(false);
+
+        phone = getIntent().getStringExtra("phone");
+
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String OTP = etOTP.getText().toString().trim();
+
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(VerificationId,OTP);
+                signInWithPhoneAuthCredential(credential);
+
+                dialog.setTitle("Verifying");
+                dialog.show();
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                Log.d("myapp","Verification Completed");
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                dialog.dismiss();
+                Toast.makeText(Verification.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+
+                VerificationId = s;
+                token = forceResendingToken;
+
+                dialog.dismiss();
+                showResend();
+            }
+        };
+
+        tvResend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvResend.setVisibility(View.GONE);
+
+                dialog.setTitle("Resending OTP");
+                dialog.show();
+
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        phone,        // Phone number to verify
+                        60,                 // Timeout duration
+                        TimeUnit.SECONDS,   // Unit of timeout
+                        Verification.this,               // Activity (for callback binding)
+                        mCallbacks,         // OnVerificationStateChangedCallbacks
+                        token);             // ForceResendingToken from callbacks
+            }
+        });
+        showResend();
+    }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            dialog.dismiss();
+                            FirebaseUser user = task.getResult().getUser();
+                            startActivity(new Intent(Verification.this,MainActivity.class));
+                            Verification.this.finish();
+                            // ...
+                        } else {
+                            dialog.dismiss();
+                            // Sign in failed, display a message and update the UI
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                Toast.makeText(Verification.this, "Wrong OTP entered", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    public void showResend()
+    {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                tvResend.setVisibility(View.VISIBLE);
+            }
+        },60000);
+    }
+}
