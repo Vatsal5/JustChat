4adc0fd65417b174eab7a65c0533e54cb351f3cb
tanishaggarwal <tanishaggarwal9357@gmail.com>
2020-02-01 23:12:04 +0530

    Merge branch 'master' of https://github.com/Vatsal5/ChattingApp
    # Conflicts:
    #	app/src/main/java/com/androidstudio/chattingapp/MainActivity.java
    #	app/src/main/java/com/androidstudio/chattingapp/UserAdapter.java

--- app/src/main/java/com/androidstudio/chattingapp/MainActivity.java
+++ app/src/main/java/com/androidstudio/chattingapp/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements UserAdapter.itemS
                         if(i==0)
                         {
                             contacts1.add(new UserDetail(contacts.get(i).getPh_number(), contacts.get(i).getuID()));
-                            (reference.child("users").child(currentUserNumber).child(contacts.get(i).getPh_number()).child("message")).setValue(null);
+                            (reference.child("users").child(currentUserNumber).child(contacts.get(i).getPh_number()).child("message")).setValue("/null");
                             c=1;
                         }
                         else
