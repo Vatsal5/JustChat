89e47ad5801cb5c8d9fe7256fd872f8db530877b
Vatsal5 <aggarwal.vatsal5@gmail.com>
2020-03-22 13:30:12 +0530

    SendingImage

--- app/src/main/java/com/androidstudio/chattingapp/UserAdapter.java
+++ app/src/main/java/com/androidstudio/chattingapp/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.androidstudio.chattingapp;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,10 @@ import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -87,7 +92,19 @@ public class UserAdapter extends ArrayAdapter<UserDetailwithUrl> {
         }
         else
         {
-            Glide.with(context).load(list.get(position).getUrl()).into(iv);}
+            Glide.with(context).load(list.get(position).getUrl()).listener(new RequestListener<Drawable>() {
+                @Override
+                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                    iv.setClickable(false);
+                    return false;
+                }
+
+                @Override
+                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                    return false;
+                }
+            }).into(iv);
+        }
 
         ConstraintLayout innerConstraintLayout = v.findViewById(R.id.innerConstraintLayout);
 
@@ -108,6 +125,8 @@ public class UserAdapter extends ArrayAdapter<UserDetailwithUrl> {
 
             }
         });
+
+
 
         iv.setOnClickListener(new View.OnClickListener() {
             @Override
