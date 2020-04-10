ce953575cecd3f587c905794691c682fcd99305a
Vatsal5 <aggarwal.vatsal5@gmail.com>
2020-04-10 15:19:20 +0530

    SendingImage

--- app/src/main/java/com/androidstudio/chattingapp/VideoActivity.java
+++ app/src/main/java/com/androidstudio/chattingapp/VideoActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideoActivity extends AppCompatActivity {
 
     VideoView vv;
+    MediaController mediaC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,12 @@ public class VideoActivity extends AppCompatActivity {
         vv=findViewById(R.id.vv);
         vv.setVideoURI(Uri.parse(getIntent().getStringExtra("uri")));
         vv.requestFocus();
+
+        mediaC = new MediaController(this);
+
+        vv.setMediaController(mediaC);
+        mediaC.setAnchorView(vv);
+
         Log.d("playuri",getIntent().getStringExtra("uri"));
         vv.start();
     }
