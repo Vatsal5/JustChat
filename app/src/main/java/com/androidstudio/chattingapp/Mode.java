cd247c141c1a4626f782ea8345e6068a7fea17d1
tanishaggarwal <tanishaggarwal9357@gmail.com>
2020-04-11 17:16:55 +0530

    notify message count

--- app/src/main/java/com/androidstudio/chattingapp/Mode.java
+++ app/src/main/java/com/androidstudio/chattingapp/Mode.java
@@ -15,10 +15,13 @@ import android.widget.TextView;
 public class Mode extends AppCompatActivity {
 
     EditText etEnterPassword, etAcceptPassword, etAcceptUsername, etEnterUsername;
-    TextView tvForgot;
+    TextView tvForgot,tvShowPassword;
     Button btnCreate, btnConfirm, btnConfirmUsername;
     Switch mode;
-     
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    LinearLayout llcreatePassword, llSelectMode, llConfirmPassword, llForgotPassword, llWrongId, llShowPassword, llWrongPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,17 +31,32 @@ public class Mode extends AppCompatActivity {
         etEnterPassword= findViewById(R.id.etEnterPassword);
         etEnterUsername= findViewById(R.id.etEnterUsername);
         tvForgot= findViewById(R.id.tvForgot);
+        tvShowPassword=findViewById(R.id.tvShowPassword);
         btnCreate= findViewById(R.id.btnCreate);
         btnConfirm=findViewById(R.id.btnConfirm);
         btnConfirmUsername=findViewById(R.id.btnConfirmUsername);
         mode=findViewById(R.id.changeMode);
+        llConfirmPassword=findViewById(R.id.confirmpassword);
+        llcreatePassword=findViewById(R.id.CreatePassword);
+        llSelectMode=findViewById(R.id.selectmode);
+        llForgotPassword=findViewById(R.id.forgotPassword);
+        llWrongId=findViewById(R.id.WrongId);
+        llWrongPassword=findViewById(R.id.WrongPassword);
+        llShowPassword=findViewById(R.id.Showpassword);
 
-        SharedPreferences pref= getApplicationContext().getSharedPreferences("Mode",0);
+         pref= getApplicationContext().getSharedPreferences("Mode",0);
         String defaultvalue=pref.getString("mode","null");
-        if(defaultvalue==null) {
-            SharedPreferences.Editor editor = pref.edit();
+        final String defaultpassword=pref.getString("password","null");
+        final String username=pref.getString("username","null");
+        editor = pref.edit();
+        if(!(defaultvalue.equals("null"))){
+
             editor.putString("mode", "public");
             editor.commit();
+        }
+        if(defaultvalue.equals("private"))
+        {
+            mode.setChecked(true);
         }
 
 
@@ -48,12 +66,32 @@ public class Mode extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked)
                 {
+                    llSelectMode.setVisibility(View.GONE);
+                  if(!(defaultpassword.equals("null")))
+                    {
+                        llcreatePassword.setVisibility(View.VISIBLE);
+                    }
+                  else{
+                      llConfirmPassword.setVisibility(View.VISIBLE);
+
+                  }
+
 
                 }
                 else
                 {
 
+                    llShowPassword.setVisibility(View.VISIBLE);
+
                 }
+            }
+        });
+
+        tvForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llConfirmPassword.setVisibility(View.GONE);
+                llForgotPassword.setVisibility(View.VISIBLE);
             }
         });
 
@@ -61,17 +99,45 @@ public class Mode extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if (!(defaultpassword.equals(etAcceptPassword.getText().toString().trim())))
+                {
+                    llWrongPassword.setVisibility(View.VISIBLE);
+                    llConfirmPassword.setVisibility(View.GONE);
+                    mode.setChecked(false);
+                }
+                else
+                {
+                   llSelectMode.setVisibility(View.VISIBLE);
+                   llConfirmPassword.setVisibility(View.GONE);
+                   editor.putString("mode","private");
+
+                }
+
             }
         });
         btnConfirmUsername.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                llForgotPassword.setVisibility(View.GONE);
 
+                if(username.equals(etAcceptUsername.getText().toString().trim()))
+                {
+                    llShowPassword.setVisibility(View.VISIBLE);
+                    tvShowPassword.setText("Your Password Is "+defaultpassword);
+                }
+                else{
+                    llWrongId.setVisibility(View.VISIBLE);
+                }
             }
         });
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                editor.putString("password",etEnterPassword.getText().toString().trim());
+                editor.putString("username",etEnterUsername.getText().toString().trim());
+                llSelectMode.setVisibility(View.VISIBLE);
+                llcreatePassword.setVisibility(View.GONE);
 
             }
         });
