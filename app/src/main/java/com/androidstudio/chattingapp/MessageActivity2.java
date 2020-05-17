2b51dc5bb8cd3b2531717e3ddd70e34bb0649d70
Vatsal5 <aggarwal.vatsal5@gmail.com>
2020-05-17 14:50:45 +0530

    bugs

--- app/src/main/java/com/androidstudio/chattingapp/MessageActivity2.java
+++ app/src/main/java/com/androidstudio/chattingapp/MessageActivity2.java
@@ -1170,6 +1170,7 @@ public class MessageActivity2 extends AppCompatActivity implements MessageAdapte
                         break;
                     case "rename":
                         messageModel.setMessage("Group title has been changed to '" + detail + "'");
+                        groupName = detail;
                         tvTitle.setText(detail);
                         ApplicationClass.rename=detail;
                         break;
