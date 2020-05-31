027acac73c36bb0eb291ce9a91abf9702bb4e13a
Vatsal5 <aggarwal.vatsal5@gmail.com>
2020-05-31 12:38:52 +0530

    Merge branch 'master' of https://github.com/Vatsal5/ChattingApp
    # Conflicts:
    #	app/src/main/java/com/androidstudio/chattingapp/MessageActivity.java

--- app/src/main/java/com/androidstudio/chattingapp/Profile.java
+++ app/src/main/java/com/androidstudio/chattingapp/Profile.java
@@ -529,7 +529,7 @@ public class Profile extends AppCompatActivity implements profile_listitem_adapt
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
-                        Profile.this.finish();
+                        dialog.dismiss();
 
                     }
                 });
