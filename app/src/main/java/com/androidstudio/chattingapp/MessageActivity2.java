5d96ad076ea27eb199bcac29ba10c3019e7eba1d
tanishaggarwal <tanishaggarwal9357@gmail.com>
2020-06-08 17:07:37 +0530

    Merge branch 'master' of https://github.com/Vatsal5/ChattingApp
    # Conflicts:
    #	app/src/main/java/com/androidstudio/chattingapp/MessageActivity2.java

--- app/src/main/java/com/androidstudio/chattingapp/MessageActivity2.java
+++ app/src/main/java/com/androidstudio/chattingapp/MessageActivity2.java
@@ -250,25 +250,7 @@ public class MessageActivity2 extends AppCompatActivity implements MessageAdapte
 
     private void sendFCMPush(String message,String to) {
 
-        FirebaseInstanceId.getInstance().getInstanceId()
-                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w("tag", "getInstanceId failed", task.getException());
-                            return;
-                        }
-
-                        // Get new Instance ID token
-                        String token = task.getResult().getToken();
-                        FirebaseDatabase.getInstance().getReference().child("tokens").child(sender).setValue(token);
-
-                        // Log and toast
-                        // String msg = getString(R.string.msg_token_fmt, token);
-                        // Log.d("tag", msg);
-                        // Toast.makeText(MessageActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    }
-                });
+       
 
         final String Legacy_SERVER_KEY = "AIzaSyBdu42ejssWEllOGpOlDYiEnlZRkWD1rgI";
         String msg = etMessage.getText().toString();
